2b31cb3c7ba59d6473d08a01a0300ef7f3ea4753
skiprox <seanmichaelscanlan@gmail.com>
2018-03-20 00:15:33 -0400

    Removing greensock

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // Greensock
-require('./../../node_modules/gsap/src/uncompressed/TweenLite.js');
-require('./../../node_modules/gsap/src/uncompressed/easing/EasePack.js');
-require('./../../node_modules/gsap/src/uncompressed/plugins/CssPlugin.js');
-require('./../../node_modules/gsap/src/uncompressed/plugins/ScrollToPlugin.js');
-require('./../../node_modules/gsap/src/uncompressed/TimelineLite.js');
+// require('./../../node_modules/gsap/src/uncompressed/TweenLite.js');
+// require('./../../node_modules/gsap/src/uncompressed/easing/EasePack.js');
+// require('./../../node_modules/gsap/src/uncompressed/plugins/CssPlugin.js');
+// require('./../../node_modules/gsap/src/uncompressed/plugins/ScrollToPlugin.js');
+// require('./../../node_modules/gsap/src/uncompressed/TimelineLite.js');
 
 // All other modules
 const example = require('./_js/example');
