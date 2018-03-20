3f044305e123cd284d8999fc52d9fe3536cfe314
skiprox <seanmichaelscanlan@gmail.com>
2018-03-19 23:19:34 -0400

    Adding in ticker functionality

--- src/assets/_js/_data/animations.js
+++ src/assets/_js/_data/animations.js
@@ -1,5 +1,23 @@
 module.exports = {
 	tileAnimation: function(elem) {
-		console.log('we got tiles');
+		let boxes = [].slice.call(elem.querySelectorAll('.box'));
+		let i = boxes.length;
+		while (i--) {
+			TweenLite.set(boxes[i], {
+				opacity: 0,
+				y: 200,
+				scaleX: 0.6,
+				scaleY: 0.6
+			})
+			TweenLite.to(boxes[i], 1, {
+				ease: Power3.easeOut,
+				opacity: 1,
+				y: 0,
+				scaleX: 1,
+				scaleY: 1,
+				delay: parseFloat(boxes[i].getAttribute('data-delay') || 0)
+			})
+		}
+		console.log('we got tiles', boxes);
 	}
 }
--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+// Greensock
+require('tween-lite');
+require('ease-pack');
+require('css-plugin');
+require('scroll-to-plugin');
+require('timeline-lite');
+
+// All other modules
 const example = require('./_js/example');
 const scrollAnimations = require('./_js/scroll-animations');
 
