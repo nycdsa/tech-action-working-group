d3f10ed2b3977f116e1be53605ac0a4202ffe2fb
skiprox <seanmichaelscanlan@gmail.com>
2018-03-20 00:05:53 -0400

    manually linking greensock

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // Greensock
-require('tween-lite');
-require('ease-pack');
-require('css-plugin');
-require('scroll-to-plugin');
-require('timeline-lite');
+require('./../../node_modules/gsap/src/uncompressed/TweenLite.js');
+require('./../../node_modules/gsap/src/uncompressed/easing/EasePack.js');
+require('./../../node_modules/gsap/src/uncompressed/plugins/CssPlugin.js');
+require('./../../node_modules/gsap/src/uncompressed/plugins/ScrollToPlugin.js');
+require('./../../node_modules/gsap/src/uncompressed/TimelineLite.js');
 
 // All other modules
 const example = require('./_js/example');
