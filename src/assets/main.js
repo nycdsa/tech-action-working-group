ea3b3b22ac08a89b4fa13905713a6afc3e3f4b08
Hasan Khan <theotherkhan@gmail.com>
2026-02-16 16:45:06 -0500

    trying main.js gut, removing dist from .gitignore

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,13 +1,11 @@
 'use strict';
 
 // All other modules
-const scrollAnimations = require('./_js/scroll-animations');
-const blueskyPosts = require('./_js/bluesky-posts');
+
 
 class Main {
 	constructor() {
-		new scrollAnimations();
-		new blueskyPosts();
+
 	}
 }
 
